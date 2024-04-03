4f9e807b41b0e6c7e418f898110d2129b070a0b2
Siqek <113335428+Siqek@users.noreply.github.com>
2024-04-03 10:08:05 +0200

    Changes.

--- details.php
+++ details.php
@@ -3,7 +3,7 @@
 
     include "./functions.php";
 
-    if (!isLogged() && isset($_POST["zadanieID"]))
+    if (!isLogged() || !isset($_POST["zadanieID"]))
     {
         navigateTo('./');
     }
@@ -22,29 +22,38 @@
     <?php include "./menu.php"; ?>
     <div id='content'>
         <a href="./listaZadan.php">powrót</a>
-        <div id='zadanie'>
-            <?php
-                $conn = newConn();
+        <span class='center'>
+            <div id='zadanie'>
+                <?php
+                    $conn = newConn();
 
-                $sql = "SELECT * FROM `zadania` JOIN `statusy` ON `zadania`.status = `statusy`.id WHERE `zadania`.id =" . $_POST["zadanieID"];
-                $result = mysqli_query($conn, $sql);
+                    $sql = "SELECT * FROM `zadania` JOIN `statusy` ON `zadania`.status = `statusy`.id WHERE `zadania`.id =" . $_POST["zadanieID"];
+                    $result = mysqli_query($conn, $sql);
 
-                if (mysqli_num_rows($result) > 0)
-                {
-                    if ($row = mysqli_fetch_assoc($result))
+                    if (mysqli_num_rows($result) > 0)
                     {
-                        echo "<p>" . $row["tytul"] . "</p>";
-                        echo "<p>" . $row["opis"] . "</p>";
-                        echo "<p>" . $row["nazwa"] . "</p>";
-                        echo "<p>" . $row["user"] . "</p>";
-                        echo "<p>" . $row["pracownik"] . "</p>";
-                        echo "<p>" . $row["data"] . "</p>";
+                        if ($row = mysqli_fetch_assoc($result))
+                        {
+                            echo "<span class='info'>
+                                <h1>" . $row["tytul"] . "</h1>
+                                <p>" . $row["nazwa"] . "</p>
+                            </span>
+                            <span class='info'>
+                                <p>" . $row["user"] . "</p>
+                                <p>" . $row["data"] . "</p>
+                            </span>
+                            <p>" . (($row["pracownik"]) ? $row["pracownik"] : "nie przypisano pracownika") . "</p>
+                            <div class='line'></div>
+                            <div id='opis'>
+                                <p>" . $row["opis"] . "</p>
+                            </div>";
+                        }
+                        #form do edycji jeżeli to ty utworzyleś to zadanie
+                        #zmiana statusu jeżeli ty jesteś przypisany do zadania
                     }
-                    #form do edycji jeżeli to ty utworzyleś to zadanie
-                    #zmiana statusu jeżeli ty jesteś przypisany do zadania
-                }
-            ?>
-        </div>
+                ?>
+            </div>
+        </span>
     </div>
 </body>
 </html>
